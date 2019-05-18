0548c5dc5198b12a82572eecffc69e91f42a204d
hackmitel <none>
2019-05-17 23:27:10 -0700

    try fix error about unwrapping UITabBarItem

--- Spring/DesignableTabBarController.swift
+++ Spring/DesignableTabBarController.swift
@@ -92,7 +92,7 @@ import UIKit
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for item in self.tabBar.items as [UITabBarItem]? {
+        for item in self.tabBar.items as [UITabBarItem] {
             if item {
                 if let image = item.image {
                     item.image = image.imageWithColor(tintColor: self.normalTint).withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
